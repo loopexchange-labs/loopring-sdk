75d4fa9817bbb39d525471a8944bf27badc9a085
leppaludi <105874219+leppaludi@users.noreply.github.com>
2023-09-10 22:12:43 +0000

    Export constants

--- packages/loopring-sdk/src/index.ts
+++ packages/loopring-sdk/src/index.ts
@@ -1,5 +1,6 @@
 export * from './lib/loopring';
 export * from './lib/personalSign';
 export * from './lib/generateKeyPair';
+export * from './lib/constants';
 export * from './lib/openapi/models/index';
 export { getEddsaSigNftOrder } from './lib/bridge/signature';
